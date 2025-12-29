2f1e997050cbb5135b3dad734ea4e2c1c191460f
Pablo Silva nuevo <pablo.silva.navarro@gmail.com>
2025-12-29 18:49:55 -0300

    primer commit del proyecto

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+//codigo de javascript para almacenar en repositorio local
+let a=5, b=10;
+let c = a + b;
+
+console.log("resultado:",c);
+
+//conectar al html y agregar un parrafo al div
+let bloque = document.getElementById("bloque");
+
+// agregaremos un parrafo con texto al div 
+
+bloque.innerHTML="<p> parrafo agregado </p>"
